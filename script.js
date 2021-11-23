bbc454f6906fce7432d4705f29a48f3c08381016
tdev88 <tracy.dev@outlook.com>
2021-11-23 14:06:17 +1100

    update

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-// Set variable to be planet name entered in to form
+// Assign planet name entered in to form to a variable
 let planet = document.getElementById("planet")
 
-// Get data according to form input
+
+// Fetch API to get planet data
 function getPlanet() {
  return fetch('https://api.le-systeme-solaire.net/rest/bodies/') 
   .then(Response => Response.json())
   .then(data => data)
 }
 
-// Get Planets
-const mapPlanets = () => {
+// Get information for chosen planet
+const findPlanets = () => {
   getPlanet()
     .then(data => {
       bodiesArray = data.bodies
@@ -19,13 +20,22 @@ const mapPlanets = () => {
   })
 }
 
+// Select DOM element and add event listener for Get Planet Info button
+document.getElementById('getPlanet').addEventListener('click', findPlanets)
 
-// Select DOM element and add event listener
-document.getElementById('getPlanet').addEventListener('click', mapPlanets)
+//Select DOM lement and add event listener for Change Theme button
+document.getElementById('changeTheme').addEventListener('click', toggleTheme)
 
-// Manipulate DOM
+//Select DOM element and add event listener for Change Theme
+function toggleTheme() {
+  let theme = document.body
+  theme.classList.toggle("bg-dark")
+  theme.classList.toggle("text-white")
+}
 
+// Manipulate DOM > Insert planetInfo in to modal
 function renderPlanet(result) {
   document.querySelector("h5.modal-title").innerText = result.englishName
   document.querySelector("h5#isPlanet").innerText = "Is Planet? " + result.isPlanet
 }
+
